1e11d5a87e21897c92fd6b04560459624d7a3348
Parthkumar Patel <parthkishan20@gmail.com>
2025-11-22 09:49:35 -0500

    name change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ selectedAlgorithm }: HeaderProps) => {
   return (
     <div className="text-center mb-8">
       <h1 className="text-4xl md:text-6xl font-bold mb-3 bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 bg-clip-text text-transparent animate-pulse">
-        Sorting Visualizer
+        SortBoard
       </h1>
       <p className="text-gray-300 text-lg">
         <span className="font-semibold text-blue-400">
